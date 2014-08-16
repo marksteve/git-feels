9a0da60828c94d073b6a105e7504e934826da555
Kat Padilla <hello@katpadi.ph>
2014-08-16 18:53:07 +0800

    Yay get args and commits now working!

--- app/views.py
+++ app/views.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import (
+  Blueprint,
+  jsonify,
+  render_template,
+  request,
+  current_app
+)
+import requests
+#from pattern.en import sentiment
 
 bp = Blueprint('pages', __name__)
 
@@ -7,9 +15,25 @@ def index():
     # Render template
     return render_template('index.html')
 
-@bp.route('/analyze', methods=['POST'])
+@bp.route('/analyze', methods=['GET'])
 def analyze():
+  # Get params
+  username = request.args.get('gh-username', '')
+  repo = request.args.get('gh-repo', '')
+
   # Get all commits from repo
-  # Tip: r.links["next"]
-  # from pattern.en import sentiment
-  return jsonify()
+  url = "https://api.github.com/repos/{}/{}/commits".format(username,repo)
+  res = requests.get(url)
+
+  current_app.logger.debug(get_sentiment())
+
+  # Iterate in each commit and save to array
+  messages = []
+  for commit in res.json() :
+    messages.append(commit['commit']['message'])
+
+  return jsonify(messages = messages)
+
+def get_sentiment():
+  x = 1 + 1
+  return x
